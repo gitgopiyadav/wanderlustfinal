8954798d00e18f310c294684b265a7ca08f4902f
delta-demo <gopiy3332@gmail.com>
2025-03-03 19:30:46 +0530

    update cloudinary id

---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "67b34f3d908804e07409204d",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
+
+initDB();
